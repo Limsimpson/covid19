e991052dccbebb78e794e410c522aad2261ed7e0
simpson <hisimpson90@gmail.com>
2021-01-10 23:43:23 +0900

    :label: Define all of types.
    Change 'any' types clearly.

---
+++ src/covid/index.ts
@@ -0,0 +1,44 @@
+export interface Country {
+  Country: string;
+  CountryCode: string;
+  Date: Date | string | number;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  Premium: any;
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+interface Global {
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+export interface CovidSummaryResponse {
+  Countries: Country[];
+  Date: Date | string | number;
+  Global: Global;
+  Message: string;
+}
+
+export interface CountrySummaryInfo {
+  Cases: number;
+  City: string;
+  CityCode: string;
+  Country: string;
+  CountryCode: string;
+  Date: Date | string | number;
+  Lat: string;
+  Lon: string;
+  Province: string;
+  Status: string;
+}
+
+export type CountrySummaryResponse = CountrySummaryInfo[];
